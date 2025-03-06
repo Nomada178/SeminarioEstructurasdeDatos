9a0c57aaf71e8a3918c8bf2acbcfc8ad68432180
Alejandro Garcia <lex.contacto96@gmail.com>
2025-03-06 14:56:07 -0600

    Update and rename Problema2_Sobrecargadeoperadores.cpp to Problema2_SO.cpp

---
+++ Problema2_SO.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+using namespace std;
+
+class Materia {
+public:
+    int Clave;
+    string Nombre;
+    string ProfesorTit;
+    string LibroTexto;
+
+    Materia(int clave, string nombre, string profesor, string libro)
+        : Clave(clave), Nombre(nombre), ProfesorTit(profesor), LibroTexto(libro) {}
+
+    void Imprime() const {
+        cout << "Clave: " << Clave << endl;
+        cout << "Nombre: " << Nombre << endl;
+        cout << "Profesor Titular: " << ProfesorTit << endl;
+        cout << "Libro de Texto: " << LibroTexto << endl;
+    }
+
+    Materia& operator=(int nuevaClave) {
+        Clave = nuevaClave;
+        return *this;
+    }
+
+    Materia& operator=(const string& nuevoProfesor) {
+        ProfesorTit = nuevoProfesor;
+        return *this;
+    }
+};
+
+int main() {
+    Materia prog(101, "Programacion", "Jorge Perez", "C++ Basico");
+    Materia bd(102, "Bases de Datos", "Maria Gomez", "SQL Fundamentos");
+
+    int opcion, nuevaClave;
+    string nuevoProfesor;
+
+    do {
+        cout << "\nMenu de opciones:" << endl;
+        cout << "1. Cambiar clave de Programacion" << endl;
+        cout << "2. Cambiar profesor de Bases de Datos" << endl;
+        cout << "3. Imprimir datos de Bases de Datos" << endl;
+        cout << "4. Salir" << endl;
+        cout << "Seleccione una opcion: ";
+        cin >> opcion;
+        cin.ignore();
+
+        switch (opcion) {
+            case 1:
+                cout << "Ingrese la nueva clave de Programacion: ";
+                cin >> nuevaClave;
+                prog = nuevaClave;
+                break;
+            case 2:
+                cout << "Ingrese el nuevo profesor de Bases de Datos: ";
+                cin.ignore();
+                getline(cin, nuevoProfesor);
+                bd = nuevoProfesor;
+                break;
+            case 3:
+                bd.Imprime();
+                break;
+            case 4:
+                cout << "Saliendo del programa..." << endl;
+                break;
+            default:
+                cout << "Opcion invalida." << endl;
+        }
+    } while (opcion != 4);
+
+    return 0;
+}
+
