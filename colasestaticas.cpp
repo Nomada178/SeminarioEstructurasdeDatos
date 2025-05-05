c646591951e5da8e9bffcc82f4dbbc235d7ff5f9
Alejandro Garcia <lex.contacto96@gmail.com>
2025-05-05 16:24:15 -0600

    Create colasestaticas.cpp

---
+++ colasestaticas.cpp
@@ -0,0 +1,23 @@
+
+#include <iostream>
+#include "Cola.h"
+
+int main() {
+    Cola MiCola;
+    persona x;
+
+    std::cout << "=== Registro de solicitudes ===\n";
+    for (int i = 1; i <= 5; i++) {
+        std::cout << "\nSolicitud #" << i << ":\n";
+        std::cin >> x;
+        MiCola.enqueue(x);
+    }
+
+    std::cout << "\n=== Constancias elaboradas ===\n";
+    for (int i = 1; i <= 5; i++) {
+        persona& atendido = MiCola.dequeue();
+        std::cout << atendido;
+    }
+
+    return 0;
+}
